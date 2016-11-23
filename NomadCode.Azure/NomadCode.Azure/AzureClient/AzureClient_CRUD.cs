b55ae6308a58d1d0d4261b578fe195dd13bc2514
Colby Williams <colbylwilliams@gmail.com>
2016-11-23 14:17:05 -0500

    docs

--- NomadCode.Azure/NomadCode.Azure/AzureClient/AzureClient_CRUD.cs
+++ NomadCode.Azure/NomadCode.Azure/AzureClient/AzureClient_CRUD.cs
@@ -12,6 +12,10 @@ namespace NomadCode.Azure
 
 #if OFFLINE_SYNC_ENABLED
 
+		/// <summary>
+		/// Pushes all local changes, then pulls all remote changes
+		/// </summary>
+		/// <typeparam name="T">The Table to sync.</typeparam>
 		public async Task SyncAsync<T> ()
 			where T : AzureEntity, new()
 		{
@@ -20,6 +24,11 @@ namespace NomadCode.Azure
 
 #endif
 
+		/// <summary>
+		/// Gets object of type T with an Id matching the itemId param
+		/// </summary>
+		/// <param name="itemId">The items Id.</param>
+		/// <typeparam name="T">The Table to query.</typeparam>
 		public async Task<T> GetAsync<T> (string itemId)
 			where T : AzureEntity, new()
 		{
@@ -27,6 +36,11 @@ namespace NomadCode.Azure
 		}
 
 
+		/// <summary>
+		/// Gets the items.
+		/// </summary>
+		/// <param name="where">Where.</param>
+		/// <typeparam name="T">The Table to query.</typeparam>
 		public async Task<List<T>> GetAsync<T> (Expression<Func<T, bool>> where = null)
 			where T : AzureEntity, new()
 		{
@@ -34,6 +48,11 @@ namespace NomadCode.Azure
 		}
 
 
+		/// <summary>
+		/// Finds the first item returned by the where expression.
+		/// </summary>
+		/// <param name="where">Where.</param>
+		/// <typeparam name="T">The Table to query.</typeparam>
 		public async Task<T> FirstOrDefault<T> (Expression<Func<T, bool>> where)
 			where T : AzureEntity, new()
 		{
@@ -41,6 +60,11 @@ namespace NomadCode.Azure
 		}
 
 
+		/// <summary>
+		/// Inserts or updates the items.
+		/// </summary>
+		/// <param name="item">Item.</param>
+		/// <typeparam name="T">The Table to save to.</typeparam>
 		public async Task SaveAsync<T> (T item)
 			where T : AzureEntity, new()
 		{
@@ -48,6 +72,11 @@ namespace NomadCode.Azure
 		}
 
 
+		/// <summary>
+		/// Inserts or updates the items in the list.
+		/// </summary>
+		/// <param name="items">Items.</param>
+		/// <typeparam name="T">The Table to save to.</typeparam>
 		public async Task SaveAsync<T> (List<T> items)
 			where T : AzureEntity, new()
 		{
@@ -55,6 +84,11 @@ namespace NomadCode.Azure
 		}
 
 
+		/// <summary>
+		/// Deletes the item with the Id.
+		/// </summary>
+		/// <param name="itemId">Item identifier.</param>
+		/// <typeparam name="T">The Table to query.</typeparam>
 		public async Task DeleteAsync<T> (string itemId)
 			where T : AzureEntity, new()
 		{
@@ -66,6 +100,11 @@ namespace NomadCode.Azure
 		}
 
 
+		/// <summary>
+		/// Deletes the items.
+		/// </summary>
+		/// <param name="item">Item to delete.</param>
+		/// <typeparam name="T">The Table to query.</typeparam>
 		public async Task DeleteAsync<T> (T item)
 			where T : AzureEntity, new()
 		{
@@ -73,6 +112,11 @@ namespace NomadCode.Azure
 		}
 
 
+		/// <summary>
+		/// Deletes the items.
+		/// </summary>
+		/// <param name="items">Items to delete</param>
+		/// <typeparam name="T">The Table to query.</typeparam>
 		public async Task DeleteAsync<T> (List<T> items)
 			where T : AzureEntity, new()
 		{
@@ -80,6 +124,11 @@ namespace NomadCode.Azure
 		}
 
 
+		/// <summary>
+		/// Deletes the items returned by the where expression.
+		/// </summary>
+		/// <param name="where">Where expression</param>
+		/// <typeparam name="T">The Table to query.</typeparam>
 		public async Task DeleteAsync<T> (Expression<Func<T, bool>> where = null)
 			where T : AzureEntity, new()
 		{
